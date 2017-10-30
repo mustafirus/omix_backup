dc1a1930f2cbb3458b2c94d63ea7e96e9d5fe2fe
mustafirus <golubev@nt.net.ua>
2017-10-30 05:54:52 +0200

    stage

--- backup.py
+++ backup.py
@@ -1,23 +1,128 @@
+#!/usr/bin/python3
 from datetime import datetime
 import json
 import os
-from subprocess import call, PIPE, STDOUT, check_call, check_output, CalledProcessError, Popen, DEVNULL, run
+from signal import signal, SIGINT, pause
+from subprocess import PIPE, STDOUT, Popen, DEVNULL, run
 import threading
-from time import sleep, strftime, localtime
-
-import sys
+from threading import Lock, Event
+from time import sleep
 
 backup_config = list()
 omix_cloud_dest = 'pm1.ssc.bla:rpool/misc/omix-backup'
 confdir = '.conf'
 logdir = '.log'
-devnull = open(os.devnull, 'w')
-dryrun = True
-port_is_free = 'ss -tln | grep -q ":9000 "; echo $?'
+sync_lock = Lock()
+shutdown = Event()
+
+
+class Shutdown(Exception):
+    pass
 
 
 class Dummy(object):
+    def __init__(self):
+        self.returncode = 0
+
+    @staticmethod
+    def wait():
+        return 0
+
+
+def _log_error(msg):
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "error: ", msg.strip())
+
+
+def _log_info(msg):
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "info: ", msg.strip())
+
+
+def _log_returncode(code):
+    if code == 255:
+        return
+    if code > 0:
+        _log_error("remote_sync: something wrong see log")
+    if code < 0:
+        _log_error("killed by signal: {}".format(code))
+
+
+def loadconfig():
+    with open(confdir+"/omix_backup.json", 'r') as conffile:
+        global backup_config
+        backup_config = json.load(conffile)
     pass
+
+
+def check_shutdown():
+    if shutdown.is_set():
+        raise Shutdown()
+
+
+def logfilename(parts):
+    return "{}/{}_{}.log".format(logdir, '_'.join(parts), datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+
+
+def check_cmd_is_running(host, cmd):
+    ps = run(['ssh', "root@" + host, 'pgrep -fx "{}"'.format(cmd)], stdout=DEVNULL, stderr=STDOUT)
+    return ps.returncode == 0
+
+
+def check_fs(host, fs):
+    ps = run(['ssh', "root@" + host, 'zfs list', fs], stdout=DEVNULL, stderr=STDOUT)
+    return ps.returncode == 0
+
+
+def check_and_create_fs(host, fs):
+    if not check_fs(host, fs):
+        run(['ssh', "root@" + host, 'zfs create -po mountpoint=none', fs], check=True)
+
+
+def remote_script(host=None, script=None, args=None):
+    shell = Popen(["ssh", "root@" + host, "bash", "-s", "--"] + (args or []),
+                  stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    (out, err) = shell.communicate(script)
+    if shell.returncode != 0:
+        _log_error("remote script: %s: %s!" % (host, err))
+        return ''
+    return out
+
+
+def remote_sync_cmd(host, cmd, log):
+    if host and cmd:
+        proc = Popen(["ssh", "root@" + host, "bash -s --"],
+                     stdin=PIPE, stdout=log, stderr=STDOUT, universal_newlines=True)
+        proc.stdin.write(cmd)
+        proc.stdin.close()
+        sleep(1)
+        proc.poll()
+    else:
+        proc = Dummy()
+        proc.returncode = 0
+    return proc
+
+
+def remote_sync(send_host, send_cmd, recv_host, recv_cmd, log):
+    _log_info("remote sync send: from {} cmd: {}...".format(send_host, send_cmd))
+    _log_info("remote sync recv: to   {} cmd: {}...".format(recv_host, recv_cmd))
+
+    with sync_lock:
+        recv = remote_sync_cmd(recv_host, recv_cmd, log)
+        if recv.returncode is not None:
+            _log_error("remote_sync: recv cant start see log")
+            return False
+        send = remote_sync_cmd(send_host, send_cmd, log)
+        _log_info("remote sync wait for transfer: {}".format(recv_host))
+        remote_script(host=recv_host, script=timeout_port + free_up_port, args=["30"])
+        _log_info("remote sync transfer begun: {}".format(recv_host))
+
+        # TODO test close logfile at this point
+    send.wait()
+    recv.wait()
+
+    _log_returncode(send.returncode)
+    _log_returncode(recv.returncode)
+
+    return send.returncode == 0 and recv.returncode == 0
 
 
 class Dataset(object):
@@ -27,9 +132,16 @@ class Dataset(object):
         self.src_path = path
         (self.dest_host, self.dest_path) = dest.split(':')
         self.dest_host = host if self.dest_host == 'localhost' else self.dest_host
-        self._update()
+        self.dest_exists = None
+        self.snap = None
+        self.start = None
+        self.last = None
+        self.interval = None
+        self.next = None
+        self.next_update = None
+        self.update()
 
-    def _update(self):
+    def update(self):
         self.dest_exists = check_fs(self.dest_host, self.dest_path)
         if not self.dest_exists:
             self._del_snap_src()
@@ -40,9 +152,10 @@ class Dataset(object):
             if params["omix_sync_start"] else 0
         self.last = params["omix_sync_time"]
         self.interval = self._interval_to_timestamp(params["omix_sync_interval"] or '99999d')
-        self.next = self.last + self.interval
+        self.next = self.last + self.interval  # TODO make daily at night
         if self.start > self.next:
             self.next = self.start
+        self.next_update = datetime.now().timestamp() + 3600
         pass
 
     @staticmethod
@@ -53,8 +166,11 @@ class Dataset(object):
         return q*k
 
     def _test(self):
-        cmd_test_send = "nc -zw5 {} 9000".format(self.dest_host)
-        cmd_test_recv = "nc -w5 -lp 9000"
+        cmd_test_send = "nc -zw10 {} 9000".format(self.dest_host)
+        cmd_test_recv = free_up_port + "nc -w10 -lp 9000"
+
+        if self.src_host == self.dest_host:
+            return True
 
         return remote_sync(
                 send_host=self.src_host,
@@ -85,9 +201,11 @@ class Dataset(object):
         run(['ssh', "root@" + self.src_host, zfs_rename.format(fs=self.src_path)], check=True)
 
     def _sync(self, cmd_send, cmd_recv, log):
-        mbuf_send = "| mbuffer -q -s 128k -m 1G -O {}:9000 -W 5".format(self.dest_host)  # -W 30
-        mbuf_recv = "mbuffer -q -s 128k -m 1G -I 9000 | "  # -W 30
-        mbuf_loc = "| mbuffer -q -s 128k -m 1G -W 30 |"
+        mbuf_send = "| mbuffer -q -s 128k -m 1G -O {}:9000 -W 300".format(self.dest_host)
+        mbuf_recv = "mbuffer -q -s 128k -m 1G -I 9000 -W 300 | "
+        mbuf_loc = "| mbuffer -q -s 128k -m 1G -W 300 |"
+        if log:
+            self._log_cmd(log, cmd_send, cmd_recv)
         if self.src_host != self.dest_host:
             return remote_sync(send_host=self.src_host, send_cmd=cmd_send + mbuf_send,
                                recv_host=self.dest_host, recv_cmd=mbuf_recv + cmd_recv,
@@ -98,115 +216,58 @@ class Dataset(object):
                                log=log)
         pass
 
+    def _find_last_snap(self):
+        # TODO rewite for checking omix_sync on src and dest
+        dest_snaps = remote_script(host=self.dest_host,
+                                   script="zfs list -rHtsnap -oname -Screation $1 | sed -e's/^.*@/@/'",
+                                   args=[self.dest_path])
+        dest_snaps = dest_snaps.strip().split('\n')
+        for snap in dest_snaps:
+            if check_fs(self.src_host, self.src_path + snap):
+                self.snap = snap
+                break
+        pass
+
+    def _log_cmd(self, logfile, c1, c2):
+        logfile.write("begin sync\n")
+        logfile.write(c1 + "\n")
+        logfile.write(c2 + "\n")
+        logfile.flush()
+
     def run(self):
         if self.next > datetime.now().timestamp():
-            return False
+            return
 
+        _log_info("sync client: {} fs: {}".format(self.client, self.src_path))
         if not self.dest_exists:
             parent = os.path.dirname(self.dest_path)
             check_and_create_fs(self.dest_host, parent)
 
         # test connection
-        if self.src_host != self.dest_host:
-            if not self._test():
-                return
+        if not self._test():
+            return
 
-        # cmd_send = "zfs snap {}@omix_send\n".format(self.src_path) if self.snap == "@omix_sync" else ""
-        # mbuf_send = "| mbuffer -q -s 128k -m 1G -O {}:9000 -W 5".format(self.dest_host)  # -W 30
-        # mbuf_recv = "mbuffer -q -s 128k -m 1G -I 9000 | "  # -W 30
-        # mbuf_loc = "| mbuffer -q -s 128k -m 1G -W 30 |"
-        with open(logfilename((self.client, os.path.basename(self.src_path))), 'w') as logfile:
+        with open(logfilename((self.client, os.path.basename(self.src_path))), 'w', buffering=1) as logfile:
             cmd_recv = "zfs recv -uvF {}".format(self.dest_path)
+            if check_cmd_is_running(self.dest_host, cmd_recv):
+                _log_info("sync client: {} fs: {} already runing: next try in 10 min"
+                          .format(self.client, self.src_path))
+                self.next = datetime.now().timestamp() + 600
+                return
             if not self.dest_exists:
                 cmd_send = "zfs send -pv {}{} ".format(self.src_path, self.snap)
-                self._sync(cmd_send=cmd_send, cmd_recv=cmd_recv, log=None)
-                self._update()
+                if not self._sync(cmd_send=cmd_send, cmd_recv=cmd_recv, log=logfile):
+                    return
+                self.update()
+
+            if not self.last:
+                self._find_last_snap()
 
             self._snap()
             cmd_send = "zfs send -pv {}@omix_send -I {} ".format(self.src_path, self.snap)
-            self._sync(cmd_send=cmd_send, cmd_recv=cmd_recv, log=None)
-            self._rename_snap()
-        self._update()
-
-
-def logfilename(parts):
-    return "%s/%s_%s.log" % (logdir, '_'.join(parts), strftime("%Y-%m-%d_%H:%M:%S", localtime()))
-
-
-def check_fs(host, fs):
-    ps = run(['ssh', "root@" + host, 'zfs list', fs], stdout=DEVNULL, stderr=STDOUT)
-    return ps.returncode == 0
-
-
-def check_and_create_fs(host, fs):
-    if not check_fs(host, fs):
-        run(['ssh', "root@" + host, 'zfs create -po mountpoint=none', fs], check=True)
-
-
-def remote_script(host=None, script=None, args=None):
-    shell = Popen(["ssh", "root@" + host, "bash", "-s", "--"] + (args or []),
-                  stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    # if shutdown.is_set():
-    #     return
-    (out, err) = shell.communicate(script)
-    if shell.returncode != 0:
-        _log_error("remote script: %s: %s!" % (host, err))
-        return ''
-    return out
-
-
-def remote_sync(send_host, send_cmd, recv_host, recv_cmd, log):
-    _log_info("remote sync send: from {} cmd: {}...".format(send_host, send_cmd))
-    _log_info("remote sync recv: to   {} cmd: {}...".format(recv_host, recv_cmd))
-    if recv_host and recv_cmd:
-        recv = Popen(["ssh", "root@" + recv_host, "bash", "-s", "--"],
-                     stdin=PIPE, stdout=log, stderr=STDOUT, universal_newlines=True)
-        recv._stdin_write(recv_cmd)
-        sleep(1)
-        recv.poll()
-        if recv.returncode is not None:
-            _log_error("remote_sync: recv something wrong see log")
-            return
-    else:
-        recv = Dummy()
-        recv.returncode = 0
-    send = Popen(["ssh", "root@" + send_host, "bash", "-s", "--"],
-                 stdin=PIPE, stdout=log, stderr=STDOUT, universal_newlines=True)
-    send.communicate(send_cmd)  # exception
-    if recv_host and recv_cmd:
-        recv.wait()
-
-    if send.returncode == 0 and recv.returncode == 0:
-        return True
-    if send.returncode > 0 or recv.returncode > 0:
-        _log_error("remote_sync: something wrong see log")
-    if send.returncode < 0:
-        _log_error("remote_sync: send killed by signal: {}".format(send.returncode))
-    if recv.returncode < 0:
-        _log_error("remote_sync: recv killed by signal: {}".format(recv.returncode))
-
-    pass
-    return False
-
-
-def _log_error(msg):
-    print("error: ", msg.strip())
-
-
-def _log_info(msg):
-    print("info: ", msg.strip())
-
-
-def loadconfig():
-    with open(confdir+"/omix_backup.json", 'r') as conffile:
-        global backup_config
-        backup_config = json.load(conffile)
-    pass
-
-
-def client_backup_vm(vm):
-    _log_info("start backup vm: %s:%s" % (vm['src_host'], vm['vmid']))
-    sleep(1)
+            if self._sync(cmd_send=cmd_send, cmd_recv=cmd_recv, log=logfile):
+                self._rename_snap()
+        self.update()
 
 
 def client_backup(client):
@@ -214,7 +275,8 @@ def client_backup(client):
 
     dest = client['dest'] if client['dest'] != 'omix_cloud' else omix_cloud_dest
     datasets = []
-
+# TODO check nc,mbuffer installed
+# TODO backup <vmid>.conf
     for src in client["src"]:
         path = src.get("path")
         vmid = src.get("vmid")
@@ -240,20 +302,35 @@ def client_backup(client):
                 pass
         else:
             _log_error("unknown backup src: " + str(src))
+    if len(datasets) == 0:
+        _log_error("nothing to do: " + str(client['client']))
+        return
+    while not shutdown.is_set():
+        next_run = None
+        _log_info("next run for client: " + client['client'])
+        for dataset in datasets:
+            dataset.run()  # TODO periodicaly rerun _update for changes of interval
+            next_run = min(next_run, dataset.next) if next_run else dataset.next
+            if shutdown.is_set():
+                return
 
-    for dataset in datasets:
-        dataset.run()
-        pass
-    pass
+        for dataset in datasets:
+            if datetime.now().timestamp() > dataset.next_update:
+                dataset.update()
+
+        s = next_run - datetime.now().timestamp()
+        while s > 0:
+            sleep(1)
+            if shutdown.is_set():
+                return
+            s -= 1
+        sleep(1)
 
 
 def start():
     loadconfig()
     check_and_create_fs(*omix_cloud_dest.split(':'))
 
-    for client in backup_config:
-        client_backup(client)
-    return
     client_threads = list()
     for client in backup_config:
         t = threading.Thread(target=client_backup, name=client['client'], args=(client,))
@@ -296,7 +373,7 @@ s/\(.*\)/pvesm path \1 /
 " | bash - | sed -ne's,^/\(dev/zvol/\)\?,zfs list -Ho name ,p' | bash - 2>/dev/null
 '''
 
-get_dataset_params = '''
+get_dataset_params = r'''
 dataset=$1
 snaps=$(zfs list -rHptsnap -o name -s creation $dataset)
 origin=$(zfs get -Hpo value origin $dataset)
@@ -323,56 +400,30 @@ cat <<EOF
 }
 EOF
 '''
-# import signal
-# def signal_handler(signal, frame):
-#     print('You pressed Ctrl+C!')
-#     shutdown.set()
-#    sys.exit(0)
+timeout_port = r'''T=$1+1; while ((T-=1)); do ss -tlnp | grep -q ' *:9000 ' || break; sleep 1; done; '''
+free_up_port = r'''ss -tlnp | awk '$4~/\*:9000/ { print gensub(/^.*,pid=([0-9]*),.*$/,"\\1","g",$6)}' \
+| xargs -r kill;'''
+
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    shutdown.set()
+
 
 if __name__ == "__main__":
-    # with open(logfilename('zzz'), 'w') as lll:
-    #     remote_command_call(src_host='localhost', cmd='/home/golubev/dev/omix_backup/tmp/z', log=lll)
-    # print remote_script(src_host='pm2.mebel.bla', script=get_vm_disks_script, args=['11004'])
+    # zzz = remote_script(host="pm1.ssc.bla", script=free_up_port)
     # exit(0)
-    #
-    # signal.signal(signal.SIGINT, signal_handler)
-    # a = remote_sync(send_host="pm1.mebel.bla",
-    #                 send_cmd="ls",
-    #                 recv_host=None,
-    #                 recv_cmd=None,
-    #                 log=None)
-    # exit(0)
+    signal(SIGINT, signal_handler)
 
-    try:
-        start()
-    except KeyboardInterrupt:
-        sys.exit(0)
-#    signal.pause()
+    start()
+    # try:
+    #     while True:
+    #         sleep(5)
+    # except KeyboardInterrupt:
+    #     sys.exit(0)
+    pause()
     pass
-
-
     # while not shutdown.is_set():
     #     sleep(3)
     #     for client in backup_config:
     #         client_backup(client)
-
-    # return
-    # backup_client(backup_config[0])
-    # return
-# def get_vm_disks(host, vmid):
-#     cmd = "ssh root@%s '%s' " % (host, get_vm_disks_script + vmid)
-#     try:
-#         check_output(cmd, shell=True, executable='/bin/bash')
-#     except CalledProcessError as e:
-#         _log_error("get_vm_disks: " + str(e.output))
-#     pass
-#     pass
-
-
-# def remote_command_call(host=None, cmd=None, log=None):
-#     cmd = "ssh root@%s '%s' 3>&2 2>&1 1>&3 3>&- | tee >(cat 1>&2)" % (host, cmd)
-#     try:
-#         check_call(cmd, stderr=log, shell=True, executable='/bin/bash')
-#     except CalledProcessError as e:
-#         _log_error("check_and_create_fs: " + str(e.output))
-#     pass
