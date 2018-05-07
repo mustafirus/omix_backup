129eca0adf4a1d471f1dfa661d25cc4ec6f45efc
mustafirus <golubev@nt.net.ua>
2018-05-08 00:29:30 +0300

    rep service

---
+++ omix_rep_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+install -o root -g root -m 744 omix_rep.py /usr/local/sbin/omix_rep.py
+install -o root -g root -m 744 omix_ssh-copy-id.py /usr/local/sbin/omix_ssh-copy-id.py
+install -o root -g root -m 644 omix_rep.service /etc/systemd/system/omix_rep.service
+install -o root -g root -m 644 omix_rep.syslog /etc/rsyslog.d/omix_rep.conf
+install -o root -g root -m 644 omix_rep.logrotate /etc/logrotate.d/omix_rep
+systemctl daemon-reload
+systemctl restart rsyslog
+systemctl enable omix_rep
+systemctl start omix_rep
